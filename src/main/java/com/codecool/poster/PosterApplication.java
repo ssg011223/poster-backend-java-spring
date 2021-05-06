1d69fbae20b0457a736964920c8199903117b8f4
Solya Gergo <solya.gergo.sandor@gmail.com>
2021-05-06 11:18:48 +0200

    disabled spring auto-login

--- src/main/java/com/codecool/poster/PosterApplication.java
+++ src/main/java/com/codecool/poster/PosterApplication.java
@@ -2,8 +2,9 @@ package com.codecool.poster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class PosterApplication {
 
 	public static void main(String[] args) {
