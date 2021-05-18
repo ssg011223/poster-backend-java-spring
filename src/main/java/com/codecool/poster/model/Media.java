09659f5b28ed0376f2368db7d464130326a06796
Solya Gergo <solya.gergo.sandor@gmail.com>
2021-05-18 16:19:57 +0200

    fix: added Media id generated value

--- src/main/java/com/codecool/poster/model/Media.java
+++ src/main/java/com/codecool/poster/model/Media.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
@@ -17,6 +14,7 @@ import javax.persistence.ManyToOne;
 @Entity
 public class Media {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @ManyToOne
