f194e0bfe7cee6578114b01299b3169ed7da03dc
Solya Gergo <solya.gergo.sandor@gmail.com>
2021-06-01 14:37:52 +0200

    refactor: SecurityConfig authenticationManagerBean and PasswordEncoder

---
+++ src/main/java/com/codecool/poster/security/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.codecool.poster.security.config;
+
+import com.codecool.poster.security.jwt.JwtFilter;
+import com.codecool.poster.security.jwt.JwtService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtService jwtService;
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        /*http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/registration**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin();*/
+
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/auth/login").permitAll();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
