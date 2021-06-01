8600cd7d4cdadd7fe2186f310cc0a748d0674679
Solya Gergo <solya.gergo.sandor@gmail.com>
2021-06-01 11:00:21 +0200

    add: UserRole

--- src/main/java/com/codecool/poster/model/UserRole.java
+++ src/main/java/com/codecool/poster/model/UserRole.java
@@ -1,7 +1,23 @@
 package com.codecool.poster.model;
 
-public enum UserRole {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-    USER,
-    ADMIN
+import javax.persistence.*;
+
+@Entity
+@Table(name = "auth_role")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserRole {
+    @Id
+    @ManyToOne
+    private Person personId;
+    @Id
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum userRole;
 }
