b340d0e93fc62535948392902996b2125bee3fd5
Solya Gergo <solya.gergo.sandor@gmail.com>
2021-06-02 17:40:55 +0200

    add: query for posts by people

--- src/main/java/com/codecool/poster/repository/PostRepository.java
+++ src/main/java/com/codecool/poster/repository/PostRepository.java
@@ -2,6 +2,12 @@ package com.codecool.poster.repository;
 
 import com.codecool.poster.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.List;
+
+@Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
+    public List<Post> findAllByPersonIdIn(Collection<Long> person_id);
 }
