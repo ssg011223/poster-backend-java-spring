3f6f11fcea242bfafe12a16fa266d514048efd00
Solya Gergo <solya.gergo.sandor@gmail.com>
2021-06-03 14:35:45 +0200

    add: share save endpoint and service

---
+++ src/main/java/com/codecool/poster/service/ShareService.java
@@ -0,0 +1,25 @@
+package com.codecool.poster.service;
+
+import com.codecool.poster.model.Share;
+import com.codecool.poster.repository.ShareRepository;
+import com.codecool.poster.security.jwt.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+
+@Service
+public class ShareService {
+    @Autowired
+    private ShareRepository shareRepository;
+    @Autowired
+    private JwtService jwtService;
+
+    public void saveShare(HttpServletRequest req, long postId) {
+        String token = jwtService.getTokenFromRequest(req);
+        long personId = jwtService.parseIdFromTokenInfo(token);
+        Share share = new Share(postId, personId, LocalDateTime.now());
+        shareRepository.save(share);
+    }
+}
